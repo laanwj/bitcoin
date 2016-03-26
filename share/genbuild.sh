8fef47b64438265b35fd9b956f6c7db761b4b6be
Wladimir J. van der Laan <laanwj@gmail.com>
2016-03-26 14:11:16 +0100

    build: remove `BUILD_DATE` / `CLIENT_DATE`
    Continues #7732 on the core side.
    
    This gets rid of a few ugly constructions, and build date is useless
    anyhow in the (most common) case of gitian builds.

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -45,7 +45,4 @@ fi
 # only update build.h if necessary
 if [ "$INFO" != "$NEWINFO" ]; then
     echo "$NEWINFO" >"$FILE"
-    if [ -n "$LAST_COMMIT_DATE" ]; then
-        echo "#define BUILD_DATE \"$LAST_COMMIT_DATE\"" >> "$FILE"
-    fi
 fi
--- src/clientversion.h
+++ src/clientversion.h
@@ -59,8 +59,6 @@ static const int CLIENT_VERSION =
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
-extern const std::string CLIENT_DATE;
-
 
 std::string FormatFullVersion();
 std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
