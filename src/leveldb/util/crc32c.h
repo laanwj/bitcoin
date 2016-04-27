eefe61c8215e00773d90e4503782b37167e589ae
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-27 14:30:04 +0200

    leveldb SSE42 CRC32C test
    Replace leveldb's CRC32C implementation with special SSE4.2 instructions.

--- src/leveldb/util/crc32c.h
+++ src/leveldb/util/crc32c.h
@@ -7,6 +7,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <nmmintrin.h>
 
 namespace leveldb {
 namespace crc32c {
@@ -14,7 +15,54 @@ namespace crc32c {
 // Return the crc32c of concat(A, data[0,n-1]) where init_crc is the
 // crc32c of some string A.  Extend() is often used to maintain the
 // crc32c of a stream of data.
-extern uint32_t Extend(uint32_t init_crc, const char* data, size_t n);
+//
+
+// Byte-boundary alignment issues
+#define CRC_ALIGN_SIZE          0x08UL              // Align at an 8-byte boundary
+#define CRC_ALIGN_MASK          (CRC_ALIGN_SIZE - 1)    // Bitmask for 8-byte bound addresses
+
+// Performs H/W CRC operations
+#define CALC_CRC(op, crc, type, buf, len)                                                                    \
+    do {                                                                                                     \
+        for (; (len) >= sizeof(type); (len) -= sizeof(type), buf += sizeof(type)) {                          \
+            (crc) = op((crc), *(type *) (buf));                                                              \
+        }                                                                                                    \
+    } while(0)
+
+/**
+ * Calculates CRC-32C using hardware support
+ * (by Anand Suresh)
+ *
+ * @param initialCrc The initial CRC to use for the operation
+ * @param buf The buffer that stores the data whose CRC is to be calculated
+ * @param len The size of the buffer
+ * @return The CRC-32C of the data in the buffer
+ */
+static inline uint32_t Extend(uint32_t initialCrc, const char *buf, size_t len) {
+    uint32_t crc = initialCrc;
+
+    // If the string is empty, return the initial crc
+    if (len == 0) return crc;
+
+    // XOR the initial CRC with INT_MAX
+    crc ^= 0xFFFFFFFF;
+
+    // Align the input to the word boundary
+    for (; (len > 0) && ((size_t)buf & CRC_ALIGN_MASK); len--, buf++) {
+        crc = _mm_crc32_u8(crc, *buf);
+    }
+
+    // Blast off the CRC32 calculation on hardware
+#if defined(__x86_64__) || defined(_M_X64)
+    CALC_CRC(_mm_crc32_u64, crc, uint64_t, buf, len);
+#endif
+    CALC_CRC(_mm_crc32_u32, crc, uint32_t, buf, len);
+    CALC_CRC(_mm_crc32_u16, crc, uint16_t, buf, len);
+    CALC_CRC(_mm_crc32_u8, crc, uint8_t, buf, len);
+
+    // XOR again with INT_MAX
+    return (crc ^= 0xFFFFFFFF);
+}
 
 // Return the crc32c of data[0,n-1]
 inline uint32_t Value(const char* data, size_t n) {
