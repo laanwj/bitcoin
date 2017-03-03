48c12e4b36cc47ffdfe59389f597136dd3b95b9d
Wladimir J. van der Laan <laanwj@gmail.com>
2017-03-03 15:44:49 +0000

    secp256k1: non-portable hacks to make tests run on cloudabi

--- src/secp256k1/src/tests.c
+++ src/secp256k1/src/tests.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 
 #include <time.h>
+#include "argdata.h"
 
 #include "secp256k1.c"
 #include "include/secp256k1.h"
@@ -37,6 +38,8 @@
 #  define VG_CHECK(x,y)
 # endif
 #endif
+
+#include <cloudabi_syscalls.h>
 
 static int count = 64;
 static secp256k1_context *ctx = NULL;
@@ -4391,47 +4394,62 @@ void run_ecdsa_openssl(void) {
 # include "modules/recovery/tests_impl.h"
 #endif
 
-int main(int argc, char **argv) {
+void program_main(const argdata_t *ad) {
     unsigned char seed16[16] = {0};
     unsigned char run32[32] = {0};
-    /* find iteration count */
-    if (argc > 1) {
-        count = strtol(argv[1], NULL, 0);
+    argdata_map_iterator_t it;
+    argdata_map_iterate(ad, &it);
+    const argdata_t *key, *value;
+
+    if (cloudabi_sys_random_get(seed16, sizeof(seed16)) != 0) {
+        uint64_t t = time(NULL) * (uint64_t)1337;
+        seed16[0] ^= t;
+        seed16[1] ^= t >> 8;
+        seed16[2] ^= t >> 16;
+        seed16[3] ^= t >> 24;
+        seed16[4] ^= t >> 32;
+        seed16[5] ^= t >> 40;
+        seed16[6] ^= t >> 48;
+        seed16[7] ^= t >> 56;
     }
 
-    /* find random seed */
-    if (argc > 2) {
-        int pos = 0;
-        const char* ch = argv[2];
-        while (pos < 16 && ch[0] != 0 && ch[1] != 0) {
-            unsigned short sh;
-            if (sscanf(ch, "%2hx", &sh)) {
-                seed16[pos] = sh;
-            } else {
-                break;
+    while (argdata_map_next(&it, &key, &value)) {
+        const char *keystr;
+        if (argdata_get_str_c(key, &keystr) == 0) {
+            if (strcmp(keystr, "count") == 0) {
+                argdata_get_int(value, &count);
+            } else if (strcmp(keystr, "seed") == 0) {
+                const char *ch;
+                if (argdata_get_str_c(value, &ch) == 0) {
+                    int pos = 0;
+                    while (pos < 16 && ch[0] != 0 && ch[1] != 0) {
+                        unsigned short sh;
+                        if (sscanf(ch, "%2hx", &sh)) {
+                            seed16[pos] = sh;
+                        } else {
+                            break;
+                        }
+                        ch += 2;
+                        pos++;
+                    }
+                }
+            } else if (strcmp(keystr, "terminal") == 0) {
+                int fd = -1;
+                FILE *fp;
+                argdata_get_fd(value, &fd);
+                if ((fp = fdopen(fd, "w")) != NULL) {
+                    fswap(fp, stderr);
+                    fclose(fp);
+                    setvbuf(stderr, NULL, _IONBF, 0);
+                }
             }
-            ch += 2;
-            pos++;
         }
-    } else {
-        FILE *frand = fopen("/dev/urandom", "r");
-        if ((frand == NULL) || !fread(&seed16, sizeof(seed16), 1, frand)) {
-            uint64_t t = time(NULL) * (uint64_t)1337;
-            seed16[0] ^= t;
-            seed16[1] ^= t >> 8;
-            seed16[2] ^= t >> 16;
-            seed16[3] ^= t >> 24;
-            seed16[4] ^= t >> 32;
-            seed16[5] ^= t >> 40;
-            seed16[6] ^= t >> 48;
-            seed16[7] ^= t >> 56;
-        }
-        fclose(frand);
     }
+
     secp256k1_rand_seed(seed16);
 
-    printf("test count = %i\n", count);
-    printf("random seed = %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", seed16[0], seed16[1], seed16[2], seed16[3], seed16[4], seed16[5], seed16[6], seed16[7], seed16[8], seed16[9], seed16[10], seed16[11], seed16[12], seed16[13], seed16[14], seed16[15]);
+    fprintf(stderr, "test count = %i\n", count);
+    fprintf(stderr, "random seed = %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", seed16[0], seed16[1], seed16[2], seed16[3], seed16[4], seed16[5], seed16[6], seed16[7], seed16[8], seed16[9], seed16[10], seed16[11], seed16[12], seed16[13], seed16[14], seed16[15]);
 
     /* initialize */
     run_context_tests();
@@ -4515,11 +4533,11 @@ int main(int argc, char **argv) {
 #endif
 
     secp256k1_rand256(run32);
-    printf("random run = %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", run32[0], run32[1], run32[2], run32[3], run32[4], run32[5], run32[6], run32[7], run32[8], run32[9], run32[10], run32[11], run32[12], run32[13], run32[14], run32[15]);
+    fprintf(stderr, "random run = %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", run32[0], run32[1], run32[2], run32[3], run32[4], run32[5], run32[6], run32[7], run32[8], run32[9], run32[10], run32[11], run32[12], run32[13], run32[14], run32[15]);
 
     /* shutdown */
     secp256k1_context_destroy(ctx);
 
-    printf("no problems found\n");
-    return 0;
+    fprintf(stderr, "no problems found\n");
+    exit(0);
 }
